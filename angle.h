020f66f9346ef048b80b10ebebdd8be855d25bb1
sgruffydd-eng <sgruffydd@beechhallschool.org>
2026-04-30 15:45:54 +0100

    Add files via upload

---
+++ angle.h
@@ -0,0 +1,13 @@
+#ifndef ANGLE_H_
+#define ANGLE_H_
+
+template<typename T> class angle {
+	public:
+		T p, y, r; // pitch, yaw, roll
+
+		angle() : p(0), y(0), r(0) { }
+
+		angle(T p, T y, T r) : p(p), y(y), r(r) { }
+};
+
+#endif // ANGLE_H_
